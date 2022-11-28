c42420b314b1a10db41288fe478a865d35e1ea74
Lengyue <lengyue@lengyue.me>
2022-11-28 05:00:32 -0500

    finish v1

--- model.py
+++ model.py
@@ -1,16 +1,37 @@
+"""
+ Copyright 2022 Lengyue
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ """
+
+# Reference
+# https://github.com/PaddlePaddle/PaddleOCR/blob/release/2.6/ppocr/modeling/backbones/rec_svtrnet.py
+
 from typing import Literal, Optional
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from einops import rearrange
-from timm.models.layers import DropPath, ConvBnAct, Mlp, PatchEmbed
+from einops.layers.torch import Rearrange
+from timm.models.layers import DropPath, Mlp
 
 default_cfgs = {
     "svtr_tiny": {
         "embed_dim": [64, 128, 256, 192],
-        "depths": [3, 6, 3],
+        "depth": [3, 6, 3],
         "num_heads": [2, 4, 8],
-        "mixers": ["local"] * 6 + ["global"] * 6,
+        "mixer": ["local"] * 6 + ["global"] * 6,
     },
 }
 
@@ -39,6 +60,39 @@ class ConvMixer(nn.Module):
         x = rearrange(x, "b (h w) d -> b d h w", h=h, w=w)
         x = self.local_mixer(x)
         x = rearrange(x, "b d h w -> b (h w) d")
+        return x
+
+
+class ConvBnAct(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int = 3,
+        stride: int = 1,
+        padding: int = 0,
+        bias=False,
+        groups=1,
+        act_layer: Optional[nn.Module] = nn.GELU,
+    ) -> None:
+        super().__init__()
+
+        self.conv = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride=stride,
+            padding=padding,
+            bias=bias,
+            groups=groups,
+        )
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.act = act_layer()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.act(x)
         return x
 
 
@@ -84,14 +138,13 @@ class Attention(nn.Module):
             )
             mask_inf = torch.full((h * w, h * w), float("-inf"), dtype=torch.float32)
             mask = torch.where(mask_paddle == 1, mask_inf, mask_paddle)
-            self.mask = mask[None, None]
+            self.mask = nn.Parameter(mask[None, None], requires_grad=False)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         if self.hw is not None:
-            N = self.hw[0] * self.hw[1]
             C = self.dim
         else:
-            N, C = x.shape[-2:]
+            C = x.shape[-2:]
 
         # TODO: add flash attention
         qkv = rearrange(
@@ -147,7 +200,7 @@ class Block(nn.Module):
             proj_drop=drop,
         )
         self.drop_path = DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
-        self.norm2 = nn.LayerNorm(dim)
+        self.norm2 = nn.LayerNorm(dim, eps=1e-6)
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(
             in_features=dim,
@@ -155,12 +208,13 @@ class Block(nn.Module):
             act_layer=nn.GELU,
             drop=drop,
         )
-    
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = x + self.drop_path(self.mixer(self.norm1(x)))
         x = x + self.drop_path(self.mlp(self.norm2(x)))
 
         return x
+
 
 class PatchEmbed(nn.Module):
     """
@@ -169,12 +223,22 @@ class PatchEmbed(nn.Module):
     """
 
     def __init__(
-        self, in_channels: int = 3, embed_dim: int = 768, num_layers: int = 2
+        self,
+        img_size: tuple[int, int] = (32, 100),
+        in_channels: int = 3,
+        embed_dim: int = 768,
+        num_layers: int = 2,
     ) -> None:
         super().__init__()
 
         assert num_layers in [2, 3], "num_layers must be 2 or 3"
 
+        num_patches = (img_size[1] // (2**num_layers)) * (
+            img_size[0] // (2**num_layers)
+        )
+
+        self.img_size = img_size
+        self.num_patches = num_patches
         self.in_channels = in_channels
         self.embed_dim = embed_dim
         self.num_layers = num_layers
@@ -205,6 +269,7 @@ class PatchEmbed(nn.Module):
         x = rearrange(x, "b c h w -> b (h w) c")
         return x
 
+
 class SubSample(nn.Module):
     def __init__(
         self,
@@ -213,6 +278,8 @@ class SubSample(nn.Module):
         stride: tuple[int, int] = (2, 1),
         act_layer: Optional[nn.Module] = None,
     ) -> None:
+        super().__init__()
+
         self.conv = nn.Conv2d(
             in_channels,
             out_channels,
@@ -222,8 +289,7 @@ class SubSample(nn.Module):
         )
         self.norm = nn.LayerNorm(out_channels)
         self.act = act_layer() if act_layer is not None else nn.Identity()
-        
-    
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.conv(x)
         x = rearrange(x, "b c h w -> b (h w) c")
@@ -234,7 +300,195 @@ class SubSample(nn.Module):
 
 
 class SVTRNet(nn.Module):
-    pass
+    def __init__(
+        self,
+        img_size: tuple[int, int] = (32, 100),
+        in_channels: int = 3,
+        embed_dim: list[int] = [64, 128, 256],
+        depth: list[int] = [3, 6, 3],
+        num_heads: list[int] = [2, 4, 8],
+        mixer: list[Literal["local", "global"]] = ["local"] * 6 + ["global"] * 6,
+        local_mixer: list[tuple[int, int]] = [(7, 11)] * 3,
+        mlp_ratio: float = 4.0,
+        qkv_bias: bool = True,
+        qk_scale: Optional[float] = None,
+        drop_rate: float = 0.0,
+        last_drop: float = 0.1,
+        attn_drop_rate: float = 0.0,
+        drop_path_rate: float = 0.1,
+        out_channels: int = 192,
+        out_char_num: int = 25,
+        sub_num: int = 2,
+    ) -> None:
+        super().__init__()
+
+        self.img_size = img_size
+        self.in_channels = in_channels
+        self.embed_dim = embed_dim
+        self.out_channels = out_channels
+
+        self.patch_embed = PatchEmbed(
+            img_size=img_size,
+            in_channels=in_channels,
+            embed_dim=embed_dim[0],
+            num_layers=sub_num,
+        )
+        num_patches = self.patch_embed.num_patches
+
+        self.hw = (img_size[0] // (2**sub_num), img_size[1] // (2**sub_num))
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches, embed_dim[0]))
+        self.pos_drop = nn.Dropout(p=drop_rate)
+
+        dpr = np.linspace(0, drop_path_rate, sum(depth))
+
+        # Build layers
+        self.blocks1 = nn.Sequential(
+            *[
+                Block(
+                    dim=embed_dim[0],
+                    num_heads=num_heads[0],
+                    mixer=mixer[i],
+                    hw=self.hw,
+                    local_k=local_mixer[0],
+                    mlp_ratio=mlp_ratio,
+                    qkv_bias=qkv_bias,
+                    qk_scale=qk_scale,
+                    drop=drop_rate,
+                    attn_drop=attn_drop_rate,
+                    drop_path=dpr[i],
+                )
+                for i in range(depth[0])
+            ]
+        )
+
+        self.sub_sample1 = SubSample(
+            embed_dim[0],
+            embed_dim[1],
+        )
+
+        self.blocks2 = nn.Sequential(
+            *[
+                Block(
+                    dim=embed_dim[1],
+                    num_heads=num_heads[1],
+                    mixer=mixer[depth[0] + i],
+                    hw=(self.hw[0] // 2, self.hw[1]),
+                    local_k=local_mixer[1],
+                    mlp_ratio=mlp_ratio,
+                    qkv_bias=qkv_bias,
+                    qk_scale=qk_scale,
+                    drop=drop_rate,
+                    attn_drop=attn_drop_rate,
+                    drop_path=dpr[depth[0] + i],
+                )
+                for i in range(depth[1])
+            ]
+        )
+
+        self.sub_sample2 = SubSample(
+            embed_dim[1],
+            embed_dim[2],
+        )
+
+        self.blocks3 = nn.Sequential(
+            *[
+                Block(
+                    dim=embed_dim[2],
+                    num_heads=num_heads[2],
+                    mixer=mixer[depth[0] + depth[1] + i],
+                    hw=(self.hw[0] // 4, self.hw[1]),
+                    local_k=local_mixer[2],
+                    mlp_ratio=mlp_ratio,
+                    qkv_bias=qkv_bias,
+                    qk_scale=qk_scale,
+                    drop=drop_rate,
+                    attn_drop=attn_drop_rate,
+                    drop_path=dpr[depth[0] + depth[1] + i],
+                )
+                for i in range(depth[2])
+            ]
+        )
+
+        self.norm = nn.LayerNorm(embed_dim[2], eps=1e-6)
+
+        # Classifier head
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, out_char_num))
+        self.last_conv = nn.Conv2d(
+            embed_dim[2], out_channels, kernel_size=1, stride=1, padding=0, bias=False
+        )
+        self.hard_swish = nn.Hardswish()
+        self.dropout = nn.Dropout(p=last_drop)
+
+    def forward_features(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.patch_embed(x)
+        x = x + self.pos_embed
+        x = self.pos_drop(x)
+
+        x = self.blocks1(x)
+        x = self.sub_sample1(
+            rearrange(x, "b (h w) c -> b c h w", h=self.hw[0], w=self.hw[1])
+        )
+        x = self.blocks2(x)
+        x = self.sub_sample2(
+            rearrange(x, "b (h w) c -> b c h w", h=self.hw[0] // 2, w=self.hw[1])
+        )
+        x = self.blocks3(x)
+
+        x = self.norm(x)
+
+        return x
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.forward_features(x)
+
+        x = rearrange(x, "b (h w) c -> b c h w", h=self.hw[0] // 4, w=self.hw[1])
+
+        x = self.avg_pool(x)
+        x = self.last_conv(x)
+        x = self.hard_swish(x)
+        x = self.dropout(x)
+
+        return x
+
+
+class CTCHead(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int) -> None:
+        super().__init__()
+
+        self.fc = nn.Linear(in_channels, out_channels)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.fc(x)
+        x = x.log_softmax(dim=2)
+
+        return x
+
+
+def build_network(cfg_name: str, out_channels: int, **kwargs) -> nn.Module:
+    cfg = default_cfgs[cfg_name]
+
+    from rec_svtr import SVTRNet as SVTRGNet
+
+    backbone = SVTRGNet(**cfg, **kwargs)
+    model = nn.Sequential(
+        backbone,
+        Rearrange("b c h w -> b (h w) c"),
+        CTCHead(backbone.out_channels, out_channels),
+    )
+
+    return model
+
 
 if __name__ == "__main__":
-    model = Attention(128, hw=(8, 8), local_k=(3, 3), mixer="local")
+    model = build_network("svtr_tiny", 100)
+
+    test_input = torch.randn(1, 3, 32, 100)
+    test_target = torch.randint(0, 25, (1, 10))
+    x = model(test_input)
+    x = x.permute(1, 0, 2)
+    print(x.shape)
+
+    loss = nn.CTCLoss(blank=0, reduction="mean", zero_infinity=True)
+    loss(
+        x, test_target, torch.tensor([x.shape[0]]), torch.tensor([test_target.shape[1]])
+    )
